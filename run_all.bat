1c7bd29beb9a0eb5956f8fd3bd9cd62706933520
moaiadmhanna <moaiadmhana999@gmail.com>
2025-05-15 11:54:00 +0200

    solved script problem

--- run_all.bat
+++ run_all.bat
@@ -1,39 +1,39 @@
 @echo off
-SET "CARLA_PATH=M:\Innolab\WindowsNoEditor"
 SET "PYTHON_VERSION=python37"
 
+start cmd /k "cd /d .\PythonAPI\examples && %PYTHON_VERSION% -m pip install -r requirements.txt
 echo ============================
 echo STARTE CARLA SIMULATOR
 echo ============================
-start "" "%CARLA_PATH%\CarlaUE4.exe" -quality-level=Low
+start "" ".\CarlaUE4.exe" -RenderOffScreen
 
 echo ============================
 echo WARTE BIS CARLA AUF PORT 2000 BEREIT IST...
 echo ============================
-powershell -ExecutionPolicy Bypass -File "%CARLA_PATH%\wait_for_carla.ps1"
+powershell -ExecutionPolicy Bypass -File ".\wait_for_carla.ps1"
 
 echo ============================
 echo CARLA IST BEREIT!
 echo ============================
 
-echo WARTE 10 SEKUNDEN...
-echo ============================
-ping -n 11 127.0.0.1 >nul
+@REM echo WARTE 10 SEKUNDEN...
+@REM echo ============================
+@REM ping -n 11 127.0.0.1 >nul
 
 echo ============================
 echo INSTALLIERE REQUIREMENTS + STARTE generate_traffic.py
 echo ============================
-start cmd /k "cd /d %CARLA_PATH%\PythonAPI\examples && %PYTHON_VERSION% -m pip install -r requirements.txt && %PYTHON_VERSION% generate_traffic.py"
+start cmd /k "cd /d .\PythonAPI\examples && %PYTHON_VERSION% -m pip install -r requirements.txt && %PYTHON_VERSION% generate_traffic.py"
 
-echo ============================
-echo WARTE 10 SEKUNDEN...
-echo ============================
-ping -n 11 127.0.0.1 >nul
-echo ============================
+@REM echo ============================
+@REM echo WARTE 10 SEKUNDEN...
+@REM echo ============================
+@REM ping -n 11 127.0.0.1 >nul
+@REM echo ============================
 
 echo STARTE manual_control.py
 echo ============================
-start cmd /k "cd /d %CARLA_PATH%\PythonAPI\examples && %PYTHON_VERSION% manual_control.py --sync"
+start cmd /k "cd /d .\PythonAPI\examples && %PYTHON_VERSION% manual_control.py --res 3840x1080 --sync"
 
 echo ============================
 echo ALLES GESTARTET!
--- wait_for_carla.ps1
+++ wait_for_carla.ps1
@@ -1,6 +1,7 @@
+Write-Host 'Warte auf Verbindung zu CARLA-Port 2000...'
 while (-not (Test-NetConnection 127.0.0.1 -Port 2000).TcpTestSucceeded) {
     Start-Sleep -Seconds 5
-    Write-Host '...CARLA noch nicht bereit...'
+    Write-Host '...Port noch nicht offen...'
 }
-Write-Host 'CARLA ist bereit!'
 
+Write-Host 'CARLA-Port erreichbar...'
